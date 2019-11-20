b3626c3f064d2dac1ae9d50bdafa510eee2e4873
Agung Kurniawan Syahferdi <Agungsyahferdi69@gmail.com>
2019-11-20 17:10:30 +0700

    Menambahkan Fungsi Toast

--- app/src/main/java/com/agungriza/yukbabad/MainActivity.java
+++ app/src/main/java/com/agungriza/yukbabad/MainActivity.java
@@ -3,6 +3,9 @@ package com.agungriza.yukbabad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnToast = findViewById(R.id.btn_toast);
+        
+        btnToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Uji Coba", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
