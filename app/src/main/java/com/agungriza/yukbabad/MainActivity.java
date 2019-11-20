f9c43e362fdff0426954e32b39fb92140fd426c1
Agung Kurniawan Syahferdi <Agungsyahferdi69@gmail.com>
2019-11-20 18:05:39 +0700

    Menambahkan Fungsi Pada Send Activity

--- app/src/main/java/com/agungriza/yukbabad/MainActivity.java
+++ app/src/main/java/com/agungriza/yukbabad/MainActivity.java
@@ -2,6 +2,7 @@ package com.agungriza.yukbabad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,11 +16,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btnToast = findViewById(R.id.btn_toast);
-        
+        Button btnSend = findViewById(R.id.btn_send);
+
         btnToast.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Uji Coba", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SendActivity.class));
             }
         });
     }
